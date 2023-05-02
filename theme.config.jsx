bc692f4daa9d3dd7b70c9dfe7176d1102bf73064
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-02 20:25:05 +0530

    adding new item in navigation

---
+++ theme.config.jsx
@@ -0,0 +1,56 @@
+import { IconBrandDeno } from '@tabler/icons-react';
+
+export default {
+  footer: <p>MIT 2023 © Nextra.</p>,
+  readMore: 'Read More →',
+  primary_navigation: [
+    {
+      route: '/',
+      name: "Home"
+    },
+    {
+      route: '/post',
+      name: "Post"
+    }, 
+    {
+      route: '/docs',
+      name: "Docs"
+    },
+    {
+      route: '/about',
+      name: "About"
+    },
+    {
+      route: '/contact',
+      name: "Contact"
+    },
+
+  ],
+  social_links: [
+    {
+      name: "Facebook",
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+    {
+      name: "Twitter",
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+    {
+      name: "Linkedin",
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+    {
+      name: "Instagram",
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+    {
+      name: "Github",
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+    {
+      name: "facebook",
+      svg: <IconBrandDeno />,
+      url: "https://facebook.com/officialrajdeepsingh"
+    },
+  ]
+}
