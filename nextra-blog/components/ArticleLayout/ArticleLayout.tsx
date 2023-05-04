7f06ddeebc6978e6f3c797dd6a75f96e912b7b3a
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-04 22:50:49 +0530

    create a layout for article

--- nextra-blog/components/ArticleLayout/ArticleLayout.tsx
+++ nextra-blog/components/ArticleLayout/ArticleLayout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Box, TypographyStylesProvider } from '@mantine/core';
 
-export const DocLayout = (props: {}) => {
-  return (
-    <div>
-      <h2>Posts </h2>
-    </div>
-  )
+export function ArticleLayout({ children, frontMatter }: { children: React.ReactNode }) {
+
+  return (<Box maw={724} mx="auto">
+    <h1>Article: {frontMatter.title} </h1>
+    <TypographyStylesProvider> {children}</TypographyStylesProvider>
+  </Box>)
+
 }
