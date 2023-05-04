49d11366c182cf48f411c9c701b6666023008acd
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-04 22:52:03 +0530

    create page layout

--- nextra-blog/components/PageLayout/PageLayout.tsx
+++ nextra-blog/components/PageLayout/PageLayout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { TypographyStylesProvider, Box } from '@mantine/core';
-
-export const PageLayout = ({ children }: { children: React.ReactNode }) => {
+import type { FrontMatter } from "nextra"
+export const PageLayout = ({ children, frontMatter }: { children: React.ReactNode frontMatter:FrontMatter }) => {
   return (
     <Box maw={724} mx="auto">
+     <h1>Page : {frontMatter.title}  </h1>
       <TypographyStylesProvider> {children}</TypographyStylesProvider>
     </Box>
   )
