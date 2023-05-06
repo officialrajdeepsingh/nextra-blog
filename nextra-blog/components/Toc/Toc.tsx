71d113b89c70819e522428d544e4d63b051b10b1
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-06 19:11:56 +0530

    complete the TOC component

--- nextra-blog/components/Toc/Toc.tsx
+++ nextra-blog/components/Toc/Toc.tsx
@@ -1,77 +1,25 @@
-import { Heading } from 'nextra';
-import Link from 'next/link';
-import { createStyles, Box, Text, Group, rem } from '@mantine/core';
+import { Box, Text, Group } from '@mantine/core';
 import { IconListSearch } from '@tabler/icons-react';
+import { TocItem } from './TocItem';
 
-const useStyles = createStyles((theme) => ({
-  link: {
-    ...theme.fn.focusStyles(),
-    display: 'block',
-    textDecoration: 'none',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-    lineHeight: 1.2,
-    fontSize: theme.fontSizes.sm,
-    padding: theme.spacing.xs,
-    borderTopRightRadius: theme.radius.sm,
-    borderBottomRightRadius: theme.radius.sm,
-    borderLeft: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-      }`,
+import { ThemeContext } from '../../Provider/config';
+import { useContext } from 'react';
+import { PageOpts, ThemeConfig } from 'nextra';
 
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-    },
-  },
 
-  linkActive: {
-    fontWeight: 500,
-    borderLeftColor: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 6 : 7],
-    color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 2 : 7],
-
-    '&, &:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25)
-          : theme.colors[theme.primaryColor][0],
-    },
-  },
-}));
-
-interface TableOfContentsProps {
-  links: Heading;
-  active: string;
-}
-
-export function Toc({ links, active }: TableOfContentsProps) {
-  const { classes, cx } = useStyles();
-
-  // depth
-  // :
-  // 2
-  // id
-  // :
-  // "5-things"
-  // value
-  // :
-  // "5 Things"
-
-  const items = links.map((item) => (
-    <Link
-      href={`#${item.id}`}
-      key={item.value}
-      className={cx(classes.link, { [classes.linkActive]: active === item.link })}
-    // sx={(theme) => ({ paddingLeft: `calc(${item.depth} * ${theme.spacing.md})` })}
-    >
-      {item.value}
-    </Link>
-  ));
-
-  return links.length >= 1 ? (
-    <div>
-      <Group mb="md">
+export function Toc() {
+  const { pageOpts } = useContext<{ pageOpts?: PageOpts, themeConfig?: ThemeConfig }>(ThemeContext)
+  return <Box my={"lg"}>
+    {pageOpts?.headings && pageOpts?.headings.length >= 1 ? <>
+      <Group my={"xl"} mb="md">
         <IconListSearch size="1.1rem" stroke={1.5} />
         <Text>Table of contents</Text>
       </Group>
-      {items}
-    </div>
-  ) : ""
+      {
+        pageOpts?.headings.map(item => <TocItem item={item} />
+        )
+      }
+    </> : ""
+    }
+  </Box>
 }
