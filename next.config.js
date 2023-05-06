c7126258ccea463b8929ef9768291fb0ea2c3295
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-06 19:13:37 +0530

    adding eslint and typescript config

--- next.config.js
+++ next.config.js
@@ -11,4 +11,16 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   // reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 })
