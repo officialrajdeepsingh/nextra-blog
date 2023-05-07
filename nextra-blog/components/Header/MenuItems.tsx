649a351569aac66203feb7653b1591501cda8427
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-07 15:21:48 +0530

    Adding linkActive class on Link component

--- nextra-blog/components/Header/MenuItems.tsx
+++ nextra-blog/components/Header/MenuItems.tsx
@@ -4,30 +4,29 @@ import { ThemeContext } from '../../Provider/config';
 import { useStyles } from "./Header-style";
 import Link from 'next/link';
 import type { PageOpts, ThemeConfig } from 'nextra';
+import { useRouter } from 'next/router';
 
 export function MenuItems() {
   const { classes, cx } = useStyles();
-
-  const { pageOpts, themeConfig } = useContext<{ pageOpts?: PageOpts, themeConfig?: ThemeConfig }>(ThemeContext)
-
-  // const navigation = pageOpts.pageMap.map(item => ({ name: item.name, kind: item.kind, route: item.route }))
-
+  const router = useRouter()
+  console.log(router)
+  const { themeConfig } = useContext<{ pageOpts?: PageOpts, themeConfig?: ThemeConfig }>(ThemeContext);
 
   const items = themeConfig.primary_navigation.map((link: { name: string; route: string; }) => {
-
     return (<Link
       key={link.name}
       href={link.route}
-      className={cx(classes.link, { [classes.linkActive]: "/" === link.route })}
+      className={cx(classes.link, router.route === link.route ? classes.linkActive : "")}
     >
       {link.name}
     </Link>)
   });
 
-
   return (
     <Group className={classes.links} position='center' spacing={5}>
-      {items}
+      {
+        items
+      }
     </Group>
   );
 }
