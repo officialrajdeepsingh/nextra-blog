3ac1fa62fed88853ce6df28096df1aac51768566
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-08 22:08:41 +0530

    adding posts in themeConfig

--- nextra-blog/Provider/config.ts
+++ nextra-blog/Provider/config.ts
@@ -1,5 +1,5 @@
-
 import { PageOpts, ThemeConfig } from "nextra";
 import { createContext } from "react";
 
-export const ThemeContext = createContext<{ pageOpts?: PageOpts, themeConfig?: ThemeConfig }>({});
+export const SearchProvider = createContext<{ posts: [] }>({ posts: [] });
+export const ThemeContext = createContext<{ pageOpts?: PageOpts; themeConfig?: ThemeConfig; posts: [] }>({});
