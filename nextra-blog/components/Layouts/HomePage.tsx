203869489a027b5d52ca9327eb9d97cd3341067f
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-09 22:42:49 +0530

    remove unused code

--- nextra-blog/components/Layouts/HomePage.tsx
+++ nextra-blog/components/Layouts/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TypographyStylesProvider, Box } from '@mantine/core';
-
+// import { OramaSearch } from "../../plugins/Orama/index"
 export const HomePage = ({ children }: { children: React.ReactNode }) => {
   return (
     <Box maw={724} mx="auto">
