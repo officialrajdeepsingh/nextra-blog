a0437b35ca1dc543820a0e72dc584a7dd3721918
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-11 18:23:38 +0530

    remove the unused code

--- nextra-blog/components/Toc/Toc.tsx
+++ nextra-blog/components/Toc/Toc.tsx
@@ -1,25 +1,31 @@
-import { Box, Text, Group } from '@mantine/core';
-import { IconListSearch } from '@tabler/icons-react';
-import { TocItem } from './TocItem';
+import { Box, Text, Group } from "@mantine/core";
+import { IconListSearch } from "@tabler/icons-react";
+import { TocItem } from "./TocItem";
 
-import { ThemeContext } from '../../Provider/config';
-import { useContext } from 'react';
-import { PageOpts, ThemeConfig } from 'nextra';
-
+import { ThemeContext } from "../../Provider/config";
+import { useContext } from "react";
+import { PageOpts, ThemeConfig } from "nextra";
 
 export function Toc() {
-  const { pageOpts } = useContext<{ pageOpts?: PageOpts, themeConfig?: ThemeConfig }>(ThemeContext)
-  return <Box my={"lg"}>
-    {pageOpts?.headings && pageOpts?.headings.length >= 1 ? <>
-      <Group my={"xl"} mb="md">
-        <IconListSearch size="1.1rem" stroke={1.5} />
-        <Text>Table of contents</Text>
-      </Group>
-      {
-        pageOpts?.headings.map(item => <TocItem item={item} />
-        )
-      }
-    </> : ""
-    }
-  </Box>
+  const { pageOpts } = useContext<{
+    pageOpts?: PageOpts;
+    themeConfig?: ThemeConfig;
+  }>(ThemeContext);
+  return (
+    <Box my={"lg"}>
+      {pageOpts?.headings && pageOpts?.headings.length >= 1 ? (
+        <>
+          <Group my={"xl"} mb="md">
+            <IconListSearch size="1.1rem" stroke={1.5} />
+            <Text>Table of contents</Text>
+          </Group>
+          {pageOpts?.headings.map((item) => (
+            <TocItem key={item.id} item={item} />
+          ))}
+        </>
+      ) : (
+        ""
+      )}
+    </Box>
+  );
 }
