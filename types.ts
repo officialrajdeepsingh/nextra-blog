17f83914f43d5581a50ec0638159788cf85e32b3
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-12 21:59:45 +0530

    adding more types

--- types.ts
+++ types.ts
@@ -1,48 +1,74 @@
-/* eslint typescript-sort-keys/interface: error */
-import type { PageOpts } from 'nextra'
-import type { ReactNode } from 'react'
-import type { Components } from 'nextra/mdx'
+import type { PageOpts } from "nextra";
+import type { ReactNode } from "react";
+import type { Components } from "nextra/mdx";
+import { Image } from "@mantine/core";
+import { type } from "os";
 
 export interface NextraBlogTheme {
-  comments?: ReactNode
-  components?: Components
+  comments?: ReactNode;
+  components?: Components;
   cusdis?: {
-    appId: string
-    host?: string
-    lang: string
-  }
-  darkMode?: boolean
-  dateFormatter?: (date: Date) => string
-  footer?: ReactNode
+    appId: string;
+    host?: string;
+    lang: string;
+  };
+  darkMode?: boolean;
+  dateFormatter?: (date: Date) => string;
+  footer?: ReactNode;
   head?: ({
     meta,
-    title
+    title,
   }: {
-    meta: Record<string, any>
-    title: string
-  }) => ReactNode
+    meta: Record<string, any>;
+    title: string;
+  }) => ReactNode;
   navs?: {
-    name: string
-    url: string
-  }[]
-  postFooter?: string
-  readMore?: string
-  titleSuffix?: string
+    name: string;
+    url: string;
+  }[];
+  postFooter?: string;
+  readMore?: string;
+  titleSuffix?: string;
 }
 
-export type BlogPageOpts = PageOpts<BlogFrontMatter>
+export type BlogPageOpts = PageOpts<BlogFrontMatter>;
+export type AuthorPageOpts = PageOpts<AuthorFrontMatter>;
+export type LayoutTypes =
+  | "home"
+  | "post"
+  | "page"
+  | "posts"
+  | "tag"
+  | "author"
+  | "authors";
 
 export type BlogFrontMatter = {
-  author?: string
-  back?: string
-  date?: string
-  description?: string
-  tag?: string | string[]
-  title?: string
-  type?: 'post' | 'page' | 'posts' | 'tag'
-}
+  author?: string;
+  date?: string;
+  description: string;
+  image?: string;
+  tags?: string[];
+  title: string;
+  type: "post";
+};
+export type iconsType =
+  | "Facebook"
+  | "Twitter"
+  | "Youtube"
+  | "Linkedin"
+  | "Github"
+  | "Instagram";
 
+export type AuthorFrontMatter = {
+  name: string;
+  description?: string;
+  image?: string;
+  tags?: string[];
+  title: string;
+  type: "author";
+  social?: { name: iconsType; svg?: React.ReactNode; url: string }[];
+};
 export interface LayoutProps {
-  config: NextraBlogTheme
-  opts: BlogPageOpts
+  config: NextraBlogTheme;
+  opts: BlogPageOpts;
 }
